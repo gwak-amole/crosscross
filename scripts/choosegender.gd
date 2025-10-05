596e787c2db4f0f1ccf4053185736ed0c8d873a2
gwak-amole <gwaksophie@gmail.com>
2025-10-05 01:55:36 -0700

    refined the player gender choosing page, and cleaned up dialogue logic for the nature girl. fully implemented sprite frames with corresponding characters in character selection, so even more character customization.

--- scripts/choosegender.gd
+++ scripts/choosegender.gd
@@ -5,10 +5,24 @@ extends Control
 @onready var confirm_btn := $confirmbtn
 @onready var anim := $AnimationPlayer
 var selected : int = 0
+var last_button_before_confirm : TextureButton = null
 
 func _ready():
 	confirm_btn.disabled = true
 	first_btn.grab_focus()
+	first_btn.focus_mode = Control.FOCUS_ALL
+	second_btn.focus_mode = Control.FOCUS_ALL
+	confirm_btn.focus_mode = Control.FOCUS_ALL
+	
+	first_btn.connect("focus_entered", Callable(self, "_on_gender_btn_focus_entered").bind(first_btn))
+	second_btn.connect("focus_entered", Callable(self, "_on_gender_btn_focus_entered").bind(second_btn))
+	confirm_btn.connect("gui_input", Callable(self, "_on_confirm_btn_gui_input"))
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_up"):
+		print(" UP U P UUP UP ")
+		if confirm_btn.has_focus() and last_button_before_confirm:
+			last_button_before_confirm.grab_focus()
 
 func _on_confirmbtn_pressed() -> void:
 	if selected:
@@ -29,6 +43,7 @@ func _on_texture_button_pressed() -> void:
 	confirm_btn.disabled = false
 	highlight_button(first_btn)
 	unhighlight_button(second_btn)
+	print(last_button_before_confirm)
 
 
 func _on_texture_button_2_pressed() -> void:
@@ -37,6 +52,7 @@ func _on_texture_button_2_pressed() -> void:
 	confirm_btn.disabled = false
 	highlight_button(second_btn)
 	unhighlight_button(first_btn)
+	print(last_button_before_confirm)
 
 func highlight_button(btn: TextureButton):
 	btn.modulate = Color(1, 1, 1, 1)
@@ -45,3 +61,11 @@ func highlight_button(btn: TextureButton):
 func unhighlight_button(btn: TextureButton):
 	btn.modulate = Color(1, 1, 1, 1)
 	btn.texture_normal = btn.texture_hover
+
+func _on_gender_btn_focus_entered(btn):
+	last_button_before_confirm = btn
+
+func _on_confirm_btn_gui_input(event):
+	if event.is_action_pressed("ui_up") and last_button_before_confirm:
+		last_button_before_confirm.grab_focus()
+		get_viewport().set_input_as_handled()
